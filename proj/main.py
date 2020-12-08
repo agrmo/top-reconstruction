a8edd65d8a26382af4d0ab85e2d7f65fe26d5607
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2020-12-07 20:57:04 -0600

    BFS done?

--- proj/main.py
+++ proj/main.py
@@ -5,105 +5,130 @@ class Expression:
         self.right = right
 
     def __iter__(self):
-        self.stack = [self]
+        self.queue = list()
+        self.queue.append((list(), self))
         return self
 
     def __next__(self):
-        if len(self.stack) == 0:
+        if len(self.queue) == 0:
             raise StopIteration
-        
-        pop = self.stack.pop()
+    
+        (path, pop) = self.queue.pop(0)
 
         if type(pop) is Expression:
-            self.stack.append(pop.right)
-            self.stack.append(pop.left)
-            return pop.operation
+            self.queue.append((path + ['l'], pop.left))
+            self.queue.append((path + ['r'], pop.right))
 
-        if type(pop) is Symbol:
-            return pop.symbol
+        return (path, pop)
 
-class Symbol:
-    def __init__(self, symbol):
-        self.symbol = symbol
+    def __str__(self):
+        strep = self.operation + '('
 
-    def __iter__(self):
-        self.itered = False
-        return self
+        if type(self.left) is Expression:
+            strep += self.left.__str__()
+        else:
+            strep += self.left
 
-    def __next__(self):
-        if self.itered:
-            raise StopIteration
+        strep += ', '
 
-        self.itered = True
-        return self.symbol
+        if type(self.right) is Expression:
+            strep += self.right.__str__()
+        else:
+            strep += self.right
 
-class Problem:    
-    def get_actions(self, state, rules):
-        actions = list()
-        
-        expansion = [e for e in state]
+        strep += ')'
 
-        for lhs, rhs in rules:
-            lhs_expansion = [e for e in lhs]
-            rhs_expansion = [e for e in rhs]
+        return strep
+
+def is_equal(expression_a, expression_b):
+    if type(expression_a) is Expression and type(expression_b) is Expression:
+        expression_a_operation = expression_a.operation
+        expression_a_left = expression_a.left
+        expression_a_right = expression_a.right
+
+        expression_b_operation = expression_b.operation
+        expression_b_left = expression_b.left
+        expression_b_right = expression_b.right
+
+        operations_equal = expression_a_operation == expression_b_operation
+        left_equal = is_equal(expression_a_left, expression_b_left)
+        right_equal = is_equal(expression_a_right, expression_b_right)
+
+        return operations_equal and left_equal and right_equal
+
+    if type(expression_a) is not Expression and type(expression_b) is not Expression:
+        return expression_a == expression_b
+
+    return False    
+
+
+def replace(expression, subexpression, path):
+    if len(path) == 0:
+        return subexpression
+
+    pop = path.pop(0)
+
+    if pop == 'l':
+        return Expression(expression.operation, replace(expression.left, subexpression, path), expression.right)
+
+    if pop == 'r':
+        return Expression(expression.operation, expression.left, replace(expression.right, subexpression, path))
+
+    # Shouldn't be here.
+
+    
+def get_successors(expression, rules):
+    successors = list()
+    
+    for lhs, rhs in rules:
+        for (path, subexpression) in expression:
+            if is_equal(lhs, subexpression):
+                successor = replace(expression, rhs, path)
+                successors.append(successor)
+
+            # if is_equal(rhs, subexpression):
+            #     successor = replace(expression, lhs, path)
+            #     successors.append(successor)
             
-            for i in range(len(expansion)):
-                if i + len(lhs_expansion) <= len(expansion):
-                    maybe_match_lhs = expansion[i: i + len(lhs_expansion)]
-                    if maybe_match_lhs == lhs_expansion:
-                        actions.append([lhs, rhs])
+    return successors
 
-                # if i + len(rhs) <= len(expansion):
-                #     maybe_match_rhs = expansion[i: i + len(rhs)]
-                #     if maybe_match_rhs == rhs:
-                #         actions.append([rhs, lhs])
-                
-        return actions
-
-    def get_successors(self, state, rules):
-        successors = list()
-        
-        actions = self.get_actions(state, rules)
-
-    def is_equal(self, state, other_state):
-        expansion = [e for e in state]
-        goal_expansion = [e for e in other_state]
-        return expansion == goal_expansion
-
-
+    
 class Agent:
-    def __init__(self, problem, start_state, goal_state, rules):
-        self.problem = problem
+    def __init__(self, start_state, goal_state, rules):
         self.start_state = start_state
         self.goal_state = goal_state
         self.rules = rules
 
 class BreadthFirstSearchAgent(Agent):
     def is_equal(self):
-        stack = list()
-        stack.append((0, self.start_state))
+        queue = list()
+        queue.append((0, self.start_state))
         max_depth = 7
         
-        while len(stack) != 0:
-            (depth, pop_state) = stack.pop()
+        while len(queue) != 0:
+            (depth, pop_state) = queue.pop()
 
-            pop_expansion = [e for e in pop_state]
-
-            if self.problem.is_equal(pop_state, self.goal_state):
+            if is_equal(pop_state, self.goal_state):
                 return True
 
             if depth < max_depth:
                 depth += 1
 
+                successors = get_successors(pop_state, self.rules)
+
+                for successor in successors:
+                    queue.append((depth, successor))
+
         return False
 
 
 def main():
-    start_state = Expression('add', Symbol('a'), Symbol('a'))
-    goal_state = Expression('add', Symbol('a'), Symbol('a'))
-    rules = [[Symbol('a'), Symbol('b')], [Symbol('b'), Symbol('c')]]
-    problem = Problem()
-    agent = BreadthFirstSearchAgent(problem, start_state, goal_state, rules)
+    start_state = Expression('add', 'a', Expression('add', 'a', 'b'))
+    goal_state = Expression('add', 'a', 'c')
+    
+    rules = [[Expression('add', 'a', 'c'), 'c']]
+    
+    agent = BreadthFirstSearchAgent(start_state, goal_state, rules)
     is_equal = agent.is_equal()
     print(is_equal)
 
