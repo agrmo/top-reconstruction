eca46ac8fc9a7c2036ab5679d99aaa36900dd451
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-08 15:05:24 +0000

    Mache Funktionen, die eine CSV-Datei eines Graphen erzeugen können.

---
+++ src/graph/GraphCSV.java
@@ -0,0 +1,22 @@
+package graph;
+
+// Ein Object, die eine CSV-Datei erzeugen kann, die einen Graph
+// verköpert. Deutlich nutzbar für das Programm Gephi.
+public class GraphCSV {
+
+    public GraphCSV() {
+	
+    }
+
+    // Mache eine CSV-Datei des Graphen, gegeben eine
+    // Nachbarschaftsliste.
+    public String mache(Nachbarschaftsliche n) {
+	
+    }
+
+    // Mache eine CSV-Datei des Graphen, gegeben eine
+    // Nachbarschaftsmatrix.
+    public String mache(Nachbarschaftsmatrix n) {
+	
+    }
+}
