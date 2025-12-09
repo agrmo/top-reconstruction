ff16ed9a7731407bc0b771929368eb575a263038
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-09 11:45:23 +0000

    Zufallsgraph mit CSV! Aber es stürzt manchmal?

--- src/graph/zufalls/Main.java
+++ src/graph/zufalls/Main.java
@@ -3,12 +3,15 @@ package graph.zufalls;
 import graph.Nachbarschaftsliste;
 import graph.Nachbarschaftsmatrix;
 import graph.graphdrucker.Graphdrucker;
+import graph.graphcsv.GraphCSV;
 
 public class Main {
     public static void main(String[] args) {
 
-	Zufallsgraph z = new Zufallsgraph(10, 0.5);
+	Zufallsgraph z = new Zufallsgraph(15, 0.2);
 	Graphdrucker gd = new Graphdrucker();
 	System.out.println(gd.drucke(z.nachbarschaftsliste));
+	GraphCSV gcsv = new GraphCSV();
+	System.out.println(gcsv.macheGepheiCSV(z.nachbarschaftsliste));
     }
 }
