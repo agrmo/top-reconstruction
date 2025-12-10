7eab9e9000b99674dc28276656f271e420ff7d11
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-10 17:30:06 +0000

    Sammeln "Graphläufer" in "Graphrechner" hinein.
    Die Funktionen sind nur Berechnugen über einen Graph. Also es wäre klarer, die
    Funktionen in "Graphrechner" zu bleiben.

--- src/graph/graphrechner/Graphrechner.java
+++ src/graph/graphrechner/Graphrechner.java
@@ -1,11 +1,10 @@
 package graph.graphrechner;
 
+import graph.Nachbarschaftsliste;
+import graph.Nachbarschaftsmatrix;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import graph.Nachbarschaftsliste;
-import graph.Nachbarschaftsmatrix;
-import graph.graphlaeufer.Graphlaeufer;
 import liste.Liste;
 
 // Berechne verschiedene Eigenschaften des Graphen.
@@ -15,6 +14,96 @@ public class Graphrechner {
     public Graphrechner() {
 	
     }
+
+    // Ist die Kante von v bis b (oder umgekehrt) im Graph?
+    public boolean hatKante(Nachbarschaftsliste n, int v, int b) {
+	return n.n.get(v).contains(b) || n.n.get(b).contains(v);
+    }
+
+    // Ist die Kante von v bis b (oder umgekehrt) im Graph?
+    public boolean hatKante(Nachbarschaftsmatrix n, int v, int b) {
+	return n.n[v][b] || n.n[b][v];
+    }
+
+    // Mache einen Kurve, der die Anzahlverteilung der Kanten
+    // im Graphen zeigt.
+    // ein: Nachbarschaftsliste
+    // aus: int[][]. Erstes Zeichen ist die Anzahl der Kante. Zweites zeichen
+    // ist die Anzahl der Knoten.
+    // z.B.
+    // [[0,0],[1,5],[2,10],[3,7],[4,3],[5,1]]
+    // Keine Knote hat keine Kante. 5 Knoten besitzen eine Kante, usw. Kein
+    // Knoten besitzt 6 Kanten.
+    // z.B.
+    // [[0,1],[1,8],[2,5],[3,2],[4,1]]
+    // Diese Liste zeigt, daß es keinen Knoten ohne Kante gibt.
+    // Es gibt 8 Knoten, die eine Kante besitzen, usw.    
+    public int[][] kanteVerteilung(Nachbarschaftsliste nl) {
+
+	// Gedanke:
+	// 1. Zähle die Anzahl von Knoten aus, die keine Kante besitzen.
+	// 2. Zähle die Anzahl von Knoten aus, die eine Kante besitzen.
+	// 3. ...bis die Knoten, die die meisten Kante besitzen.
+
+	// Nehmen wir an, daß die Nachbarschaftsliste ist ohne
+	// Verdopplung aufgebaut. Ein Knoten kann Kante besitzen, die
+	// in der Nachbarschaftsliste unmittelbar bezeichnet sind. z.B.
+	// 0: [1]
+	// 1: []
+	
+	// Der Knoten 1 besitzt eine Kante, aber sie ist nicht in der
+	// Nachbarschaftsliste unmittelbar bezeichnet.  Die einfachste
+	// Lösung dafür ist genau die Nachbarschaftsliste ja zu
+	// verdoppeln, sodaß jeder Knoten zeigt unmittelbar alle seine
+	// Kante.
+
+	nl.verdoppeln();
+
+	// Eine Map der Abzählung der Kante.
+	// z.B.
+	// 0: 1
+	// 2: 5
+	// Es gibt 5 Knoten mit 2 Kanten. Fänden wir einen neuen Knoten
+	// mit 2 Kanten, dann erhöhen das Zeichen von 2.
+	// 0: 1
+	// 2: 6
+	// Es gibt 6 Knoten mit 2 Kanten.
+	HashMap<Integer,Integer> kv = new HashMap<Integer,Integer>();
+
+	// Jeder Knoten in der Nachbarschaftsliste hat schon alle
+	// seine Kante gerade gezeigt. Also wir müssen nur den Betrag
+	// der Liste aufzählen.
+
+	for (int i = 0; i < nl.n.size(); i++) {
+	    ArrayList<Integer> knoten = nl.n.get(i);
+	    int betrag = knoten.size();
+
+	    if (kv.containsKey(betrag)) {
+		// Es gibt einen anderen Knoten mit die gleiche Anzahl
+		// von Kante. Erhöhen die Zahl.
+		int anzahl = kv.get(betrag);
+		anzahl++;
+		kv.put(betrag, anzahl);		
+	    } else {
+		// Dieser Knoten ist der erste Knoten mit betrag-Zahl
+		// von Kanten.
+		kv.put(betrag, 1);
+	    }
+	}
+
+	// Schon fertig, aber wir wollen eine int[][] zeigen, nicht
+	// eine Abbildung von int bis int.
+
+	Liste l = new Liste();
+	int[][] intkv = l.nehmeArrayArrayVonAbbildung(kv);
+
+	return intkv;
+    }
+
+    // Mache einen Kurve, der die Wahrscheinlichkeitsverteilung der
+    // Kanten im Graphen zeigt.
+    // public int[][] kanteVerteilung(Nachbarschaftsmatrix nl) {	
+    //    }        
 
     // Berechne die durchschnittliche Anzahl der Kanten eines Knoten
     // dieses Graphen.
@@ -29,9 +118,8 @@ public class Graphrechner {
 	// 3. Berechne 2 * die Anzahl von Knoten.
 	// 4. Teilen sie durch.
 
-	// Nehme die Kanteverteilung von Graphenläufer.
-	Graphlaeufer gl = new Graphlaeufer();
-	int[][] kv = gl.kanteVerteilung(nl);
+	// Nehme die Kanteverteilung.
+	int[][] kv = kanteVerteilung(nl);
 
 	// Wir haben alle die Daten, den Durschnitt zu rechnen.
 	// Nennen wir den Zähler "gewichteteSumme." Der Nenner ist die
--- src/graph/graphrechner/Main.java
+++ src/graph/graphrechner/Main.java
@@ -3,13 +3,43 @@ package graph.graphrechner;
 import graph.Nachbarschaftsliste;
 import graph.Nachbarschaftsmatrix;
 import graph.druck.tgf.GraphTGF;
-import graph.graphlaeufer.Graphlaeufer;
 import graph.zufalls.Zufallsgraph;
 import liste.Liste;
 
 public class Main {
 
     public static void beispielEins() {
+	Graphrechner gr = new Graphrechner();
+	
+	int[][] paare = {{1,2},{2,0}};
+	int betrag = 3;
+	Nachbarschaftsmatrix nm = new Nachbarschaftsmatrix(paare, betrag);
+	System.out.println(gr.hatKante(nm,1,2));
+	System.out.println(gr.hatKante(nm,2,1));
+	
+	Nachbarschaftsliste nl = new Nachbarschaftsliste(paare, betrag);
+	System.out.println(gr.hatKante(nl,1,2));
+	System.out.println(gr.hatKante(nl,2,1));
+
+	int[][] verteilung = gr.kanteVerteilung(nl);
+	Liste l = new Liste();
+	String verteilungString = l.nehmeStringVonArrayArray(verteilung);
+	System.out.println(verteilungString);	
+    }
+
+    public static void beispielZwei() {
+	Zufallsgraph zg = new Zufallsgraph(10, 0.3);
+    	GraphTGF gtgf = new GraphTGF();
+	System.out.println(gtgf.macheGepheiTGF(zg.nachbarschaftsliste));
+
+	Graphrechner gr = new Graphrechner();
+	int[][] verteilung = gr.kanteVerteilung(zg.nachbarschaftsliste);
+	Liste l = new Liste();
+	String verteilungString = l.nehmeStringVonArrayArray(verteilung);
+	System.out.println(verteilungString);	
+    }        
+
+    public static void beispielDrei() {
 	Graphrechner gr = new Graphrechner();
 	GraphTGF gtgf = new GraphTGF();
 	
@@ -23,24 +53,23 @@ public class Main {
 	System.out.println(kd);	
     }
 
-    public static void beispielZwei() {
+    public static void beispielVier() {
 	// DIE ORDNUNG ITST HIER WICHTIG!!!
 	// NACH KANTEVERTEILUNG IST DER GRAPH VERDOPPELT!
 	// ALSO DIE KANTEDURCHSCHNITT WURDE VERÄNDERT!
 	// (Nicht genau ein Bug, aber man konnte einen Fehler machen...)
 	
 	Zufallsgraph zg = new Zufallsgraph(500, 0.01);
-	Graphlaeufer gl = new Graphlaeufer();
-
 	GraphTGF gtgf = new GraphTGF();
 	System.out.println(gtgf.macheGepheiTGF(zg.nachbarschaftsliste));
 
-	int[][] kv = gl.kanteVerteilung(zg.nachbarschaftsliste);
+	Graphrechner gr = new Graphrechner();
+	int[][] kv = gr.kanteVerteilung(zg.nachbarschaftsliste);
+	// Nach gr.kanteVerteilung ist zg.nachbarschaftsliste VERDOPPELT! :[
 	Liste l = new Liste();
 	String kvString = l.nehmeStringVonArrayArray(kv);
 	System.out.println(kvString);
 
-	Graphrechner gr = new Graphrechner();
 	double kd = gr.berechneKantedurchschnitt(zg.nachbarschaftsliste);
 	System.out.println(kd);
     }
