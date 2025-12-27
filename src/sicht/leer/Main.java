56711670c5f6e724377e325ae91c207beae865d6
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-27 18:58:59 +0000

    Handlung mit der Mouse

--- src/sicht/leer/Main.java
+++ src/sicht/leer/Main.java
@@ -2,6 +2,12 @@ package sicht.leer;
 
 import javax.swing.JFrame;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
+
 /*
   javac -d classes $(find src -name '*.java') \
   && java -cp classes sicht.leer.Main
@@ -13,6 +19,19 @@ public class Main {
  	JFrame frame = new JFrame();
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(1200, 600);
+
+	frame.addMouseListener(new MouseAdapter() {
+		public void mousePressed(MouseEvent e) {
+		    System.out.println("Press");
+		}
+	    });
+
+	frame.addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                System.out.println("Drag");
+            }
+        });
+	
 	frame.setVisible(true);
     }
 }
