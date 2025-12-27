c14092fc93ff4384af438b7954b0aae5ac061ca4
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-27 22:19:09 +0000

    Figur

---
+++ src/figur/Figur.java
@@ -0,0 +1,6 @@
+package figur;
+
+// Eine Figur ist ein zweidimensionaler Ding.
+public abstract class Figur {
+    
+}
--- src/koerper/Koerper.java
+++ src/koerper/Koerper.java
@@ -1,19 +1,17 @@
 package koerper;
 
-// Ein Körper ist eine Sache, die wir betrachten können.  Er besitzt
-// verschiedene Eigenschaften, die für ihn wichtig ist, seine
-// Ausdehnung zu zeigen. Die Welt sieht verschiedene Körper.  In
-// verschiedenen Metriken kann der gleiche Körper anders
-// aussehen. Also wir können nicht seine Stelle, seinen Platz oder
-// seinen Ort wie seine Eigenschaften bestimmen. Die Welt wird uns
-// zeigen, wo ein Körper steht. Trotzdem müssen wir manchmal eine
-// Eigenschaft annehmen, die nur in Euklidraum in eins, zwei, oder
-// drei Dimensionen gilt. Zum Beispiel, ein Kubus besitzt eine Länge
-// in euklidschem Raum, aber diese Länge können nach
-// Lorentz-Beschleunigung anders aussehen. Die Welt wird uns den
-// Unterschied erklären.
-//
-// Ein Körper ist eine ganze Zahl.
+// Ein Körper ist ein dreidimensionaler Ding. Wir können einen Körper
+// mit Hilfe der Sicht betrachten.  Er besitzt verschiedene
+// Eigenschaften, die für ihn wichtig ist, seine Ausdehnung zu
+// zeigen. Die Welt enthält verschiedene Körper.  In verschiedenen
+// Metriken kann der gleiche Körper anders aussehen. Also wir können
+// nicht seine Stelle, seinen Platz oder seinen Ort wie seine
+// Eigenschaften bestimmen. Die Welt wird uns zeigen, wo ein Körper
+// steht. Trotzdem müssen wir manchmal eine Eigenschaft annehmen, die
+// nur in Euklidraum in eins, zwei oder drei Dimensionen gilt. Zum
+// Beispiel, ein Kubus besitzt eine Länge in euklidischem Raum, aber
+// diese Länge können nach Lorentz-Beschleunigung anders aussehen. Die
+// Welt wird uns den Unterschied erklären.
 //
 public abstract class Koerper {
     
