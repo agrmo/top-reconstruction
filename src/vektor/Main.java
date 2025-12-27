4f9971e283ebe8a1578c4d5e23739d5defa391b8
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-27 23:37:15 +0000

    Zweivektor

--- src/vektor/Main.java
+++ src/vektor/Main.java
@@ -1,13 +1,11 @@
 package vektor;
 
-import java.util.Arrays;
-
 /*
   javac -d classes src/vektor/* && java -cp classes vektor.Main
 */
 
 public class Main {
-    public static void main(String[] args) {
+    static void beispielEins() {
 	Dreivektor va = new Dreivektor(1,2,3);
 	Dreivektor vb = new Dreivektor(1,1,1);
 	va.addiere(vb);
@@ -18,5 +16,19 @@ public class Main {
 	vb.punkt(new Dreivektor(0,5,0));
 	
 	System.out.println(vb.drucken());
+    }
+    
+    static void beispielZwei() {
+	Zweivektor va = new Zweivektor(1,2);
+	Zweivektor vb = new Zweivektor(1,1);
+	va.addiere(vb);
+	System.out.println(va.drucken());
+	System.out.println(vb.drucken());
+	va.punkt(new Zweivektor(0,5));
+	System.out.println(va.drucken());
+    }
+    
+    public static void main(String[] args) {
+	beispielZwei();
     }
 }
---
+++ src/vektor/Zweivektor.java
@@ -0,0 +1,65 @@
+package vektor;
+
+/*
+  Ein Zweivektor ist eine Liste von zwei ganzen Zahlen.
+  
+  Ein Zweivektor im Euklidraum.
+
+  (Wie berechne ich Zweivektoren im Minkowskiraum?)
+ */
+public class Zweivektor {
+    public int eins;
+    public int zwei;
+
+    // ein: ganze Zahl, ganze Zahl
+    public Zweivektor(int eins, int zwei) {
+	this.eins = eins;
+	this.zwei = zwei;
+    }
+
+    // ein:
+    // v: Zweivektor
+    // Addiere den gegebenen Zweivektor zu diesem.  
+    public void addiere(Zweivektor v) {
+	this.eins += v.eins;
+	this.zwei += v.zwei;
+    }
+
+    // ein: Zweivektor
+    // Subtrahiere den gegebenen Zweivektor zu diesem.  
+    public void subtrahiere(Zweivektor v) {
+	this.eins -= v.eins;
+	this.zwei -= v.zwei;
+    }
+
+    // ein: Zweivektor
+    // Dividiere den gegebenen Zweivektor zu diesem.  
+    public void dividiere(Zweivektor v) {
+	this.eins = this.eins / v.eins;
+	this.zwei = this.zwei / v.zwei;
+    }
+
+    // ein: Zweivektor
+    // Multipliziere ganz den gegebenen Zweivektor zu diesem.  
+    public void multipliziere(Zweivektor v) {
+	this.eins = this.eins * v.eins;
+	this.zwei = this.zwei * v.zwei;
+    }
+    
+    // ein: Zweivektor
+    // Multipliziere teilweise den gegebenen Zweivektor zu diesem.  
+    public void punkt(Zweivektor v) {
+	this.eins = this.eins * v.eins;
+	this.zwei = this.zwei * v.zwei;
+    }
+
+    public String drucken() {
+	StringBuilder sb = new StringBuilder();
+	sb.append("[");
+	sb.append(this.eins);
+	sb.append(",");
+	sb.append(this.zwei);
+	sb.append("]");
+	return sb.toString();
+    }
+}
