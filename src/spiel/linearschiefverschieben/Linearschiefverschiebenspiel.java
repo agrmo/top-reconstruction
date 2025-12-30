9a777a24aa7b7935279d95c5c1dc6e5384e73151
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2025-12-30 10:06:06 +0000

    Unwichtig

--- src/spiel/linearschiefverschieben/Linearschiefverschiebenspiel.java
+++ src/spiel/linearschiefverschieben/Linearschiefverschiebenspiel.java
@@ -1,38 +1,143 @@
 package spiel.linearschiefverschieben;
 
-import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import sicht.linearschiefverschieben.Linearschiefverschiebensicht;
-import handlung.Linearschiefverschiebentastaturhandlung;
-import welt.punktkoerper.Punktkoerperwelt;
+import spiel.Spiel;
 import punkt.Dreipunkt;
 
-// Ein Spiel mit einer Linearschiefsicht einer Punktkörperwelt mit der
-// Tastatur.  Ein linearschiefes Spiel enthält eine Handlung, eine
-// Punktkörperwelt und eine Sicht. Die Handlung enthält auch die
-// Sicht, weil sie beiden sie wiederdarstellen sollen.
-public class Linearschiefverschiebenspiel {
+// Ein Spiel, in dem wir die Welt und Sichten verändern können.
+//
+// WSAD: Verschieben die Sicht mit Hilfe der Linearschiefsicht
+// TGFH: Verschieben die ursprüngliche Punkten der Punktkörperwelt.
+// []: Verschieben die Tiefe der Schiefsicht.
+// Hoch Runter Links Rechts: Verschieben die Sicht verhältnismäßig.
+public class Linearschiefverschiebenspiel extends Spiel {
 
-    public Linearschiefverschiebenspiel() {
+    Linearschiefverschiebensicht lss;
+
+    public Linearschiefverschiebenspiel(Linearschiefverschiebensicht l) {
+	this.lss = l;
     }
 
-    public void spielen(Punktkoerperwelt pkw,
-			double a,
-			double mx, double bx, double my, double by,
-			Dreipunkt vp) {
-
-	JFrame frame = new JFrame();
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(1200, 600);
-
-	Linearschiefverschiebensicht ps = new Linearschiefverschiebensicht(pkw,
-								   a,
-								   mx, bx, my, by,
-								   vp);
+    public void handelnMouseDrucken(MouseEvent me) {
+	// nichts
+    }
+    
+    public void handelnMouseLösen(MouseEvent me) {
+	// nichts
 	
-	Linearschiefverschiebentastaturhandlung h = new Linearschiefverschiebentastaturhandlung(ps);
+    }
+    
+    public void handelnMouseEin(MouseEvent me) {
+	// nichts
+	
+    }
+    
+    public void handelnMouseAus(MouseEvent me) {
+	// nichts
+	
+    }
+    
+    public void handelnMouse(MouseEvent me) {
+	// nichts
+	
+    }
+    
+    public void handelnTastatur(KeyEvent ke) {
+	int id = ke.getID();
+	boolean veraendert = false;
+	
+        if (id == KeyEvent.KEY_TYPED) {
+            char kc = ke.getKeyChar();
 
-	frame.add(ps);
-	frame.addKeyListener(h);
-	frame.setVisible(true);	
+	    if (kc == 'w') {
+		this.lss.lv.by -= 10.0;
+		veraendert = true;
+		
+	    } else if (kc == 's') {
+		this.lss.lv.by += 10.0;
+		veraendert = true;
+		
+	    } else if (kc == 'a') {
+		this.lss.lv.bx -= 10.0;
+		veraendert = true;
+		
+	    } else if (kc == 'd') {
+		this.lss.lv.bx += 10.0;
+		veraendert = true;
+		
+	    } else if (kc == 't') {
+		Dreipunkt vp = new Dreipunkt(0,-1,0);
+		this.lss.verschiebenpunkt = (vp);
+		veraendert = true;
+
+	    } else if (kc == 'g') {
+		Dreipunkt vp = new Dreipunkt(0,1,0);
+		this.lss.verschiebenpunkt = (vp);
+		veraendert = true;
+		
+	    } else if (kc == 'f') {
+		Dreipunkt vp = new Dreipunkt(-1,0,0);
+		this.lss.verschiebenpunkt = (vp);
+		veraendert = true;
+		
+	    } else if (kc == 'h') {
+		Dreipunkt vp = new Dreipunkt(1,0,0);
+		this.lss.verschiebenpunkt = (vp);
+		veraendert = true;
+		
+	    } else if (kc == '[') {
+		this.lss.sv.a += 0.1;
+		veraendert = true;
+		
+	    } else if (kc == ']') {
+		this.lss.sv.a -= 0.1;
+		veraendert = true;
+		
+	    }
+        }
+
+	if (veraendert == true) {
+	    this.lss.repaint();
+	}
+    }
+    
+    public void handelnTastaturDrucken(KeyEvent ke) {
+	int keyCode = ke.getKeyCode();
+	boolean veraendert = false;
+	    
+	if (keyCode == 38) {
+	    // Hoch getastet.
+		
+	    this.lss.lv.my *= 0.9;
+	    veraendert = true;
+		
+	} else if (keyCode == 40) {
+	    // Runter getastet.
+		
+	    this.lss.lv.my *= 1.1;
+	    veraendert = true;
+		
+	} else if (keyCode == 37) {
+	    // Links getastet.
+
+	    this.lss.lv.mx *= 0.9;
+	    veraendert = true;
+		
+	} else if (keyCode == 39) {
+	    // Rechts getastet.
+		
+	    this.lss.lv.mx *= 1.1;
+	    veraendert = true;
+	}
+
+	if (veraendert == true) {
+	    this.lss.repaint();
+	}
+    }
+    
+    public void handelnTastaturLösen(KeyEvent ke) {
+	// nichts
     }
 }
--- src/spiel/linearschiefverschieben/Main.java
+++ src/spiel/linearschiefverschieben/Main.java
@@ -1,12 +1,17 @@
 package spiel.linearschiefverschieben;
 
+import handlung.spiel.SpielMouseHandlung;
+import handlung.spiel.SpielTastaturHandlung;
+import java.awt.Color;
 import java.util.ArrayList;
+import javax.swing.JFrame;
 import koerper.kubus.Kubus;
+import punkt.Dreipunkt;
 import punkt.Dreipunkt;
 import punktkoerper.Punktkoerper;
 import punktkoerper.kubus.Punktkubus;
+import sicht.linearschiefverschieben.Linearschiefverschiebensicht;
 import welt.punktkoerper.Punktkoerperwelt;
-import punkt.Dreipunkt;
 
 /*
   javac -d classes $(find src -type f) \
@@ -16,7 +21,7 @@ import punkt.Dreipunkt;
 public class Main {
     public static void main(String[] args) {
 	
-	// Mache eine Punktkörperwelt.
+	// Mache die Welt.
 	Kubus k = new Kubus(50);
 	Dreipunkt p = new Dreipunkt(100,100,100);
 	Punktkubus pk = new Punktkubus(k, p);
@@ -24,14 +29,34 @@ public class Main {
 	pkl.add(pk);
 	Punktkoerperwelt pkw = new Punktkoerperwelt(pkl);
 
+	// Mache die Sicht.
 	double a = 1.0;
 	double mx = 1.0;
 	double bx = 0.0;
 	double my = 1.0;
 	double by = 0.0;
 	Dreipunkt vp = new Dreipunkt(0,0,0);
+	Linearschiefverschiebensicht lss = new Linearschiefverschiebensicht(pkw,
+									    a,
+									    mx, bx, my, by,
+									    vp);
 	
-	Linearschiefverschiebenspiel s = new Linearschiefverschiebenspiel();
-	s.spielen(pkw, a, mx, bx, my, by, vp);
+	// Mache das Spiel.
+	Linearschiefverschiebenspiel s = new Linearschiefverschiebenspiel(lss);
+
+	// Mache die Handlungen.
+	SpielMouseHandlung mh = new SpielMouseHandlung(s);
+	SpielTastaturHandlung th = new SpielTastaturHandlung(s);
+
+	// Geh.
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().setBackground(Color.BLACK);
+	frame.getContentPane().setForeground(Color.WHITE);
+	frame.setSize(1200, 600);
+	frame.add(lss);
+	frame.addKeyListener(th);
+	frame.addMouseListener(mh);
+	frame.setVisible(true);	
     }
 }
