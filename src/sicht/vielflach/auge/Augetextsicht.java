b1d7b9826db41ef2bc454a47f7f75783a11000f4
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2026-04-01 18:58:48 +0000

    unwichtig

--- src/sicht/vielflach/auge/Augetextsicht.java
+++ src/sicht/vielflach/auge/Augetextsicht.java
@@ -6,6 +6,9 @@ import sicht.text.Textsicht;
 import vektor.Zweivektor;
 import sicht.Sicht;
 import druck.vektor.Vektordrucker;
+import druck.matrix.Matrixdrucker;
+import matrix.Dreimatrix;
+import matrix.rollnickgier.Rollnickgiermatrix;
 
 // Eine Sicht, die einen Text der Eigenschaften der
 // Augesicht auf dem Bildschirm schreibt.
@@ -21,12 +24,16 @@ public class Augetextsicht extends Sicht {
     }
 
     public void darstellen(Graphics g) {
+	Dreimatrix basismatrix = Rollnickgiermatrix.nehmebasisdrehungmatrix(this.as.augevektor);
+	
 	String[] tl = new String[] {
-	    Vektordrucker.drucken(this.as.augevektor)
+	    Vektordrucker.drucken(this.as.augevektor),
+	    Matrixdrucker.drucken(basismatrix)
 	};
 
 	Zweivektor[] tol = new Zweivektor[] {
-	    new Zweivektor(0,12)
+	    new Zweivektor(0,12),
+	    new Zweivektor(0,12*6)
 	};
 
 	// Ein bischen schneller. Wir benutzen die gleiche Sicht jedes Mal.
