3eb845b0d394bab5217e186516123828ea87aab6
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2026-04-21 19:44:32 +0000

    unwichtig

--- src/welt/bogen/Main.java
+++ src/welt/bogen/Main.java
@@ -2,7 +2,6 @@ package welt.bogen;
 
 import bogen.Bogen;
 import vektor.Zweivektor;
-import vektor.Dreivektor;
 
 /*
   javac -d classes $(find src -type f) \
@@ -12,11 +11,16 @@ import vektor.Dreivektor;
 public class Main {
 
     static void beispieleins() {
+	// Mache die Welt.
+	Bogen[] bl = new Bogen[] {
+	    new Bogen(60,40,0,-90),
+	    new Bogen(50,30,0,90)};
 	
-    }
-
-    static void beispielzwei() {
+	Zweivektor[] ol = new Zweivektor[] {
+	    new Zweivektor(200, 200),
+	    new Zweivektor(100, 100)};
 	
+	Zweibogenwelt zbw = new Zweibogenwelt(bl, ol);
     }
 
     public static void main(String[] args) {
