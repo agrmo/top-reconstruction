f84b7697b93506c43cc9c8ab2646c167872ba9bc
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2026-04-23 00:41:06 +0000

    hmmmm

---
+++ src/orientierung/Orientierung.java
@@ -0,0 +1,22 @@
+package orientierung;
+
+/*
+
+  Schon haben wir Code für verschiedene zweidimensionalen Figuren und
+  für verschiedene dreidimensionalen Körper.
+
+  Wir haben auch schon Code für das räumliche Verorten der
+  zweidimensionalen Figuren und dreidimensionalen Körper: die Welt.
+
+  Aber dieser Code kann nicht solche Dingen orientieren.
+
+  Zum Beispiel, wie bauen wir einen Kubus, der auf einer seinen Ecke
+  steht? Das können wir nicht Bauen. Eine Vielflachwelt kann nur den
+  Ort eines Kubus zeigen, aber nicht seine Orientierung.
+
+  Zum Beispiel, wie bauen wir einen Kegel, der auf seiner Spitze steht?
+
+ */
+public abstract class Orientierung {
+    
+}
