661508919ebaa6358f3bd5c809099f88e41038d4
Agustin Romero <89935000+agrmo@users.noreply.github.com>
2026-05-01 00:29:12 +0000

    unwichtig

--- src/sicht/vektor/zwei/Main.java
+++ src/sicht/vektor/zwei/Main.java
@@ -14,8 +14,7 @@ import welt.vektor.Zweivektorwelt;
 
 public class Main {
 
-    public static void main(String[] args){
-	
+    static void beispieleins() {
 	// Mache die Welt.
 	Zweivektor[] vektore = new Zweivektor[] {
 	    new Zweivektor(30,50)
@@ -34,6 +33,34 @@ public class Main {
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(1200, 600);
 	frame.add(m);
+	frame.setVisible(true);	
+    }
+
+    static void beispielzwei() {
+	// Mache die Welt.
+	Zweivektor[] vektore = new Zweivektor[] {
+	    new Zweivektor(30,50),
+	    new Zweivektor(10,50)
+	};
+	Zweivektor[] orte = new Zweivektor[] {
+	    new Zweivektor(100, 100),
+	    new Zweivektor(200, 100)
+	};
+	Zweivektorwelt ow = new Zweivektorwelt(vektore, orte);
+
+	// Mache die Sicht.
+	Zweivektorsicht s = new Zweivektorsicht(ow);
+	Maler m = new Maler(new Sicht[] {s});
+	
+	// Stellen die Daten dar.
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(1200, 600);
+	frame.add(m);
 	frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+	beispielzwei();
     }
 }
